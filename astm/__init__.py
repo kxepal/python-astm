18e12c648a38222f33fc877102d24cb4bf5c56cc
Alexander Shorin <Alexander Shorin@localhost>
2012-09-15 16:41:49 +0400

    Initial commit.

---
+++ astm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
