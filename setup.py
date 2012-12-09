73979433432a326f1697d3346411a4b9bbfddd58
Alexander Shorin <kxepal@gmail.com>
2012-12-09 19:01:57 +0400

    Version bump!
    0.1 release includes ASTM decode/encode methods, record mappings and demo
    implementation of Omnilab LabOnline module.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 
 setup(
     name = 'astm',
-    version = '0.0',
+    version = '0.1',
     description = 'Python implementation of ASTM E1381/1394-91 protocol.',
 
     author = 'Alexander Shorin',
